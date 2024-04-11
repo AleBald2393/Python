9ddd4117cb4f80005587f2a0372ecc3085400872
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-10 19:08:09 -0500

    Update RegEx.py

--- Teoría/RegEx.py
+++ Teoría/RegEx.py
@@ -25,3 +25,12 @@ if match:
     print("Phone number found:", match.group())
 else:
     print("No match")
+
+s2 = "Michael Jackson was a singer and known as the 'King of Pop'"
+
+
+# Use the findall() function to find all occurrences of the "as" in the string
+result = re.findall("as", s2)
+
+# Print out the list of matched words
+print(result)
