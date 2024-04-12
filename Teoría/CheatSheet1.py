6188331f55ea0c8a5588d7e823e26f0106c77c02
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-11 19:21:36 -0500

    Create CheatSheet1.py

---
+++ Teoría/CheatSheet1.py
@@ -0,0 +1,61 @@
+PARA COMENTARIOS:
+# This is a comment
+CONCATENATION:
+Syntaxis: concatenated_string = string1 + string2 
+ejemplo:result = "Hello" + " John"</td>
+DATA TYPES: 
+x=7 
+# Integer Value 
+y=12.4 
+# Float Value 
+is_valid = True 
+# Boolean Value 
+is_valid = False 
+# Boolean Value 
+F_Name = "John" 
+# String Value
+INDEXING:
+my_string="Hello" 
+char = my_string[0]
+LENGHT OF A STRING
+len(string_name) 
+EJEMPLO: my_string="Hello" 
+length = len(my_string)
+LOWERCASE:
+my_string="Hello" 
+uppercase_text = my_string.lower()
+PRINT:
+print("Hello, world") 
+print(a+b)
+PHYTON OPERATORS
+x = 9 y = 4 
+result_add= x + y # Addition 
+result_sub= x - y # Subtraction 
+result_mul= x * y # Multiplication 
+result_div= x / y # Division 
+result_fdiv= x // y # Floor Division 
+result_mod= x % y # Modulo</td>
+REPLACE:
+x = 9 y = 4 
+result_add= x + y # Addition 
+result_sub= x - y # Subtraction 
+result_mul= x * y # Multiplication 
+result_div= x / y # Division 
+result_fdiv= x // y # Floor Division 
+result_mod= x % y # Modulo</td>
+SLICING:
+substring = string_name[start:end] 
+Ejemplo; my_string="Hello" substring = my_string[0:5]
+SPLIT
+my_string="Hello" 
+split_text = my_string.split(",")
+STRIP
+my_string="Hello" 
+trimmed = my_string.strip()
+UPPER:
+my_string="Hello" 
+uppercase_text = my_string.upper()
+VARIABLE ASSIGNMENT:
+variable_name = value 
+Ejemplo: name="John" # assigning John to variable name 
+x = 5 # assigning 5 to variable x
