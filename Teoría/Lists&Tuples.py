b19f4c8800528b5603551aebead191246bb2c4ce
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-12 16:59:43 -0500

    Update Lists&Tuples.py

--- Teoría/Lists&Tuples.py
+++ Teoría/Lists&Tuples.py
@@ -8,3 +8,9 @@ L
 L = [ "Michael Jackson", 10.2]
 L.append(['pop', 10])
 L
+# Change the element based on the index
+
+A = ["disco", 10, 1.2]
+print('Before change:', A)
+A[0] = 'hard rock'
+print('After change:', A)
