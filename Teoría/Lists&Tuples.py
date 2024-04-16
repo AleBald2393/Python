f567b073707206dc9b5da5fe5db06f5b80ad78f8
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-15 20:16:26 -0500

    Update Lists&Tuples.py

--- Teoría/Lists&Tuples.py
+++ Teoría/Lists&Tuples.py
@@ -30,3 +30,22 @@ print('After change:', A)
 
 B = A[:]
 B
+
+TUPLES
+# Get the length of tuple
+
+len(tuple2)
+
+# Sort the tuple
+
+RatingsSorted = sorted(Ratings)
+RatingsSorted
+
+# Print element on each index, including nest indexes
+
+print("Element 2, 0 of Tuple: ",   NestedT[2][0])
+print("Element 2, 1 of Tuple: ",   NestedT[2][1])
+print("Element 3, 0 of Tuple: ",   NestedT[3][0])
+print("Element 3, 1 of Tuple: ",   NestedT[3][1])
+print("Element 4, 0 of Tuple: ",   NestedT[4][0])
+print("Element 4, 1 of Tuple: ",   NestedT[4][1])
