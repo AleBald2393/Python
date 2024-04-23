0f702f6df1b72a5c761f3478c8fa80cd2bf5b25e
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-22 20:08:44 -0500

    Create Loops.py

---
+++ Teoría/Loops.py
@@ -0,0 +1,29 @@
+for val in sequence:
+      # statement(s) to be executed in sequence as a part of the loop.
+
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+for color in colors:
+    print(color)
+
+for number in range(1, 11):
+    print(number)
+
+for number in range(11):
+    print(number)
+
+for number in range(1, 11):
+    print(number)
+
+fruits = ["apple", "banana", "orange"]
+for index, fruit in enumerate(fruits):
+    print(f"At position {index}, I found a {fruit}")
+
+while condition:
+    # Code to be executed while the condition is true
+    # Indentation is crucial to indicate the scope of the loop
+
+  count = 1
+while count <= 10:
+    print(count)
+    count += 1
