9c67bb3c0b2a24416b22a73c9d950674a714de68
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-04-24 20:18:43 -0500

    Update Functions.py

--- Teoría/Functions.py
+++ Teoría/Functions.py
@@ -47,3 +47,83 @@ def print_numbers(limit):
     for i in range(1, limit+1):
         print(i)
 print_numbers(5)  # Output: 1 2 3 4 5
+
+#Compare Two Strings Directly using in operator
+# add string
+string= "Michael Jackson is the best"
+
+# Define a funtion
+def check_string(text):
+    
+# Use if else statement and 'in' operatore to compare the string
+    if text in string:
+        return 'String matched'
+    else:
+        return 'String not matched'
+
+check_string("Michael Jackson is the best")
+
+#Compare two strings using == operator and function
+def compareStrings(x, y):
+# Use if else statement to compare x and y
+    if x==y:
+        return 1
+    
+# Declare two different variables as string1 and string2 and pass string in it
+string1 = "Michael Jackson is the best"
+string2 = "Michael Jackson is the best"
+
+# Declare a variable to store result after comparing both the strings
+check = compareStrings(string1, string2)
+
+#Use if else statement to compare the string
+if check==1:
+    print("\nString Matched")
+else:
+    print("\nString not Matched")
+
+# Python Program to Count words in a String using Dictionary
+def freq(string):
+    
+    #step1: A list variable is declared and initialized to an empty list.
+    words = []
+    
+    #step2: Break the string into list of words
+    words = string.split() # or string.lower().split()
+    
+    #step3: Declare a dictionary
+    Dict = {}
+    
+    #step4: Use for loop to iterate words and values to the dictionary
+    for key in words:
+        Dict[key] = words.count(key)
+        
+    #step5: Print the dictionary
+    print("The Frequency of words is:",Dict)
+    
+#step6: Call function and pass string in it
+freq("Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go")
+
+def freq(string,passedkey):
+
+    #step1: A list variable is declared and initialized to an empty list.
+    words = []
+
+    #step2: Break the string into list of words
+    words = string.split() # or string.lower().split()
+
+    #step3: Declare a dictionary
+    Dict = {}
+
+    #step4: Use for loop to iterate words and values to the dictionary
+    for key in words:
+        if(key == passedkey):
+            Dict[key] = words.count(key)   
+    #step5: Print the dictionary
+    print("Total Count:",Dict)
+
+#step6: Call function and pass string in it
+freq("Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go","little")
+    
