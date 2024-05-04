7f304d62c27ecf79b8c04c911eb1109e701b0fe3
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-04 13:20:28 -0500

    Update Numpy.py

--- Teoría/Numpy/Numpy.py
+++ Teoría/Numpy/Numpy.py
@@ -46,3 +46,88 @@ SLICING
 
 d = c[1:4]
 d
+
+# Set the fourth element and fifth element to 300 and 400
+
+c[3:5] = 300, 400
+c
+We can also define the steps in slicing, like this: [start:end:step].
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+
+print(arr[1:5:2])
+
+#print the even elements
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+# Enter your code here
+print  (arr[1:8:2])
+
+# Create the index list
+
+select = [0, 2, 3, 4]
+select
+
+# Get the size of numpy array
+
+a.size
+
+# Get the number of dimensions of numpy array
+
+a.ndim
+
+# Get the shape/size of numpy array
+
+a.shape
+
+# Get the mean of numpy array
+
+mean = a.mean()
+mean
+
+# Get the standard deviation of numpy array
+
+standard_deviation=a.std()
+standard_deviation
+
+# Get the biggest value in the numpy array
+
+max_b = b.max()
+max_b
+
+# Get the smallest value in the numpy array
+
+min_b = b.min()
+min_b
+
+# Numpy Array Addition
+
+z = np.add(u, v)
+z
+
+# Plotting functions
+
+
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+
+def Plotvec1(u, z, v):
+    
+    ax = plt.axes() # to generate the full window axes
+    ax.arrow(0, 0, *u, head_width=0.05, color='r', head_length=0.1)# Add an arrow to the  U Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(u + 0.1), 'u')#Adds the text u to the Axes 
+    
+    ax.arrow(0, 0, *v, head_width=0.05, color='b', head_length=0.1)# Add an arrow to the  v Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(v + 0.1), 'v')#Adds the text v to the Axes 
+    
+    ax.arrow(0, 0, *z, head_width=0.05, head_length=0.1)
+    plt.text(*(z + 0.1), 'z')#Adds the text z to the Axes 
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+
+# Plot numpy arrays
+
+Plotvec1(u, z, v)
