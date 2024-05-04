bfca3067282d3745b327a8818f38d842c012f378
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-04 18:08:31 -0500

    Create 2D Numpy.py

---
+++ Teoría/Numpy/2D Numpy.py
@@ -0,0 +1,49 @@
+# Import the libraries
+
+import numpy as np
+
+# Create a list
+
+a = [[11, 12, 13], [21, 22, 23], [31, 32, 33]]
+a
+
+A = np.array(a)
+A
+
+A.ndim
+
+# Show the numpy array shape
+
+A.shape
+
+# Show the numpy array size
+
+A.size
+
+# Access the element on the second row and third column
+
+A[1, 2]
+
+# Access the element on the second row and third column
+
+A[1][2]
+
+# Access the element on the second row and third column
+
+A[1][2]
+
+# Access the element on the first row and first and second columns
+
+A[0][0:2]
+
+# Access the element on the first and second rows and third column
+
+A[0:2, 2]
+
+# Calculate the sine of Z
+
+np.sin(Z)
+
+# Get the transposed of C
+
+C.T
