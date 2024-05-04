458df794e58de3439bae57b1e806261053f03315
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-04 14:04:07 -0500

    Update Numpy.py

--- Teoría/Numpy/Numpy.py
+++ Teoría/Numpy/Numpy.py
@@ -131,3 +131,103 @@ def Plotvec1(u, z, v):
 # Plot numpy arrays
 
 Plotvec1(u, z, v)
+
+c = np.subtract(a, b)
+
+print(c)
+
+# Numpy Array Multiplication
+
+z = np.multiply(x, y)
+z
+
+c = np.divide(a, b)
+c
+
+# Calculate the dot product
+
+np.dot(X, Y)
+
+# Add the constant to array
+
+u + 1
+
+# The value of pi
+
+np.pi
+
+# Create the numpy array in radians
+
+x = np.array([0, np.pi/2 , np.pi])
+
+# Calculate the sin of each elements
+
+y = np.sin(x)
+y
+
+Linspace
+A useful function for plotting mathematical functions is linspace. Linspace returns evenly spaced numbers over a specified interval.
+
+    numpy.linspace(start, stop, num = int value)
+
+start : start of interval range
+
+stop : end of interval range
+
+num : Number of samples to generate.
+
+# Makeup a numpy array within [-2, 2] and 5 elements
+
+np.linspace(-2, 2, num=5)
+
+# Make a numpy array within [-2, 2] and 9 elements
+
+np.linspace(-2, 2, num=9)
+
+# Plot the result
+
+plt.plot(x, y)
+
+Iterating 1-D Arrays
+Iterating means going through elements one by one.
+
+If we iterate on a 1-D array it will go through each element one by one.
+If we execute the numpy array, we get in the array format
+
+# Import the libraries
+
+import time 
+import sys
+import numpy as np 
+
+import matplotlib.pyplot as plt
+
+
+def Plotvec2(a,b):
+    ax = plt.axes()# to generate the full window axes
+    ax.arrow(0, 0, *a, head_width=0.05, color ='r', head_length=0.1)#Add an arrow to the  a Axes with arrow head width 0.05, color red and arrow head length 0.1
+    plt.text(*(a + 0.1), 'a')
+    ax.arrow(0, 0, *b, head_width=0.05, color ='b', head_length=0.1)#Add an arrow to the  b Axes with arrow head width 0.05, color blue and arrow head length 0.1
+    plt.text(*(b + 0.1), 'b')
+    plt.ylim(-2, 2)#set the ylim to bottom(-2), top(2)
+    plt.xlim(-2, 2)#set the xlim to left(-2), right(2)
+
+# Write your code below and press Shift+Enter to execute
+arr1=np.array([1,2,3,4,5])
+arr2=np.array([6,7,8,9,10])
+#Starting index in slice is 1 as first even element(2) in array1 is at index 1
+even_arr1 = arr1[1:5:2]
+print("even for array1",even_arr1)
+    
+#Starting index in slice is 0 as first odd element(1) in array1 is at index 0
+odd_arr1=arr1[0:5:2]
+print("odd for array1",odd_arr1)
+
+#Starting index in slice is 0 as first even element(6) in array2 is at index 0
+even_arr2 = arr2[0:5:2]
+print("even for array2",even_arr2)
+    
+    
+#Starting index in slice is 1 as first odd element(7) in array2 is at index 1
+odd_arr2=arr2[1:5:2]
+print("odd for array2",odd_arr2)
