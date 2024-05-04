3ac98106bb7a6546c39cd09752f36d4ddd027017
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-04 12:46:51 -0500

    Update Numpy.py

--- Teoría/Numpy/Numpy.py
+++ Teoría/Numpy/Numpy.py
@@ -1,1 +1,24 @@
+NumPy is a Python library used for working with arrays, linear algebra, fourier transform, and matrices. NumPy stands for Numerical Python and it is an open source project. The array object in NumPy is called ndarray, it provides a lot of supporting functions that make working with ndarray very easy.
 
+Arrays are very frequently used in data science, where speed and resources are very important.
+
+NumPy is usually imported under the np alias.
+
+It's usually fixed in size and each element is of the same type. We can cast a list to a numpy array by first importing numpy:
+
+# import numpy library
+
+import numpy as np 
+
+# Create a numpy array
+
+a = np.array([0, 1, 2, 3, 4])
+a
+
+# Print each element
+
+print("a[0]:", a[0])
+print("a[1]:", a[1])
+print("a[2]:", a[2])
+print("a[3]:", a[3])
+print("a[4]:", a[4])
