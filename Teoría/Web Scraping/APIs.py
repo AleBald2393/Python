278b5c5194c11cf55d4a4422c5c21cf9060b5baf
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-11 08:23:14 -0500

    Update APIs.py

--- Teoría/Web Scraping/APIs.py
+++ Teoría/Web Scraping/APIs.py
@@ -8,3 +8,31 @@ The disadvantage of using APIs:
 Security. If the API is poorly integrated, it means it will be vulnerable to attacks, resulting in data breeches or losses having financial or reputation implications.
 
 Another example of simple API we will use in this notebook is Fruityvice application. The Fruityvice API web service which provides data for all kinds of fruit! You can use Fruityvice to find out interesting information about fruit and educate yourself. The web service is completely free to use and contribute to.
+
+Get Methods
+get_cell()
+get_city()
+get_dob()
+get_email()
+get_first_name()
+get_full_name()
+get_gender()
+get_id()
+get_id_number()
+get_id_type()
+get_info()
+get_last_name()
+get_login_md5()
+get_login_salt()
+get_login_sha1()
+get_login_sha256()
+get_nat()
+get_password()
+get_phone()
+get_picture()
+get_postcode()
+get_registered()
+get_state()
+get_street()
+get_username()
+get_zipcode()
