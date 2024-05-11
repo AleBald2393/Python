8acb8de648098c1f2eb27838a380d8163a6d0ce4
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-11 08:20:50 -0500

    Create APIs.py

---
+++ Teoría/Web Scraping/APIs.py
@@ -0,0 +1,8 @@
+The advantages of using APIs:
+
+Automation. Less human effort is required and workflows can be easily updated to become faster and more
+productive.
+Efficiency. It allows to use the capabilities of one of the already developed APIs than to try to independently implement some functionality from scratch.
+The disadvantage of using APIs:
+
+Security. If the API is poorly integrated, it means it will be vulnerable to attacks, resulting in data breeches or losses having financial or reputation implications.
