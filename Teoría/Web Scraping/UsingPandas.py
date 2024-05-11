be596479a76d1fe2474670aa0b26dfb2fb0b5835
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-05-11 10:45:25 -0500

    Update UsingPandas.py

--- Teoría/Web Scraping/UsingPandas.py
+++ Teoría/Web Scraping/UsingPandas.py
@@ -142,3 +142,66 @@ list_input = table_bs.find_all(href="https://en.wikipedia.org/wiki/Florida")
 list_input
 
 table_bs.find_all('a', href=True)
+
+find
+%%html
+<h3>Rocket Launch </h3>
+
+<p>
+<table class='rocket'>
+  <tr>
+    <td>Flight No</td>
+    <td>Launch site</td> 
+    <td>Payload mass</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>Florida</td>
+    <td>300 kg</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>Texas</td>
+    <td>94 kg</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Florida </td>
+    <td>80 kg</td>
+  </tr>
+</table>
+</p>
+<p>
+
+<h3>Pizza Party</h3>
+  
+    
+<table class='pizza'>
+  <tr>
+    <td>Pizza Place</td>
+    <td>Orders</td> 
+    <td>Slices </td>
+   </tr>
+  <tr>
+    <td>Domino's Pizza</td>
+    <td>10</td>
+    <td>100</td>
+  </tr>
+  <tr>
+    <td>Little Caesars</td>
+    <td>12</td>
+    <td >144 </td>
+  </tr>
+  <tr>
+    <td>Papa John's </td>
+    <td>15 </td>
+    <td>165</td>
+  </tr>
+
+USING PANDAS
+import pandas as pd
+
+tables = pd.read_html(url)
+tables
+
+tables[0]
