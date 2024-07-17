e671a0376b9cdb709e39775dbcc1cb98bb440d71
AleBald2393 <166253963+AleBald2393@users.noreply.github.com>
2024-07-16 20:07:22 -0500

    Create Practice Project - Data Analytics.py

---
+++ Teoría/Practice Project - Data Analytics.py
@@ -0,0 +1,4 @@
+The following required libraries are not pre-installed in the Skills Network Labs environment. You will need to run the following cell to install them:
+import piplite
+await piplite.install('seaborn')
+
